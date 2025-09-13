b716d44e971d94d74a80a79cb056297dc616d444
ppytlarczyk <pat.pytlarczyk@gmail.com>
2025-09-13 20:52:29 +0200

    removed links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { Outlet } from 'react-router-dom';
 
@@ -8,9 +7,6 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - CI/CD practive webapp</h1>
-        <Link to="/" className="text-blue-500">Home</Link>
-        <Link to="/about" className="text-blue-500">About</Link>
-        <Link to="/contact" className="text-blue-500">Contact</Link>
       </div>
       <Outlet />
     </div>
