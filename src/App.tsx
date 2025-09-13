d32795842e6ce58f5403081044732a998c13506a
ppytlarczyk <pat.pytlarczyk@gmail.com>
2025-09-13 20:49:27 +0200

    Added mock links

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
@@ -6,6 +8,9 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - CI/CD practive webapp</h1>
+        <Link to="/" className="text-blue-500">Home</Link>
+        <Link to="/about" className="text-blue-500">About</Link>
+        <Link to="/contact" className="text-blue-500">Contact</Link>
       </div>
       <Outlet />
     </div>
