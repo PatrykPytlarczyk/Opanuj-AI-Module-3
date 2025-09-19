aff659eebaa5b09fe8fe4976117b1f66da56f097
ppytlarczyk <pat.pytlarczyk@gmail.com>
2025-09-19 21:57:24 +0200

    space to trigger commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - CI/CD practice webapp</h1>
         <h2 className="text-lg font-bold text-center">Deployed with Netlify</h2>
-        <h2 className="text-lg font-bold text-center">Deployed with Vercel</h2>
+        <h2 className="text-lg font-bold text-center">Deployed with Vercel </h2>
       </div>
       <Outlet />
     </div>
