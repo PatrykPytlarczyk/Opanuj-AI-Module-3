c6ae9f7c179b38e99aaf49f917fa29effc7e04bf
ppytlarczyk <pat.pytlarczyk@gmail.com>
2025-09-21 08:44:23 +0200

    Updated VersionBadge component to use 'fixed' positioning instead of 'absolute' for improved layout consistency.

--- src/components/VersionBadge.tsx
+++ src/components/VersionBadge.tsx
@@ -5,7 +5,7 @@ const {VITE_ENVIRONMENT} = import.meta.env
 const VersionBadge = () => {
 
   return (
-    <div className="d-flex g-8 absolute right-4 bottom-8 py-4 px-8 bg-white border-2">
+    <div className="d-flex g-8 fixed right-4 bottom-8 py-4 px-8 bg-white border-2">
 
       <span>Environment: {VITE_ENVIRONMENT ?? 'localhost'} | Version: {__APP_VERSION__}</span>
     </div>
